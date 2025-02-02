1534ea81e2f33f110c826af5777f21654b7c0ca0
Meggur <shvenk.stepan@mail.ru>
2025-02-03 01:01:55 +0700

    Initial commit

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="h-[40px] bg-white shadow-lg flex ">
+            <div className={"w-1/2 h-full bg-[#2b4264]"}>
+            </div>
+            <div className={"w-1/2 h-full bg-[#E1C042]"}>
+            </div>
+        </div>
+    );
+};
+export default Footer;
